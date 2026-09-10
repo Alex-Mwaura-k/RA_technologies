0920909e391755f116f20a9e7186a713f377245e
Alex Mwaura <alecsmwaura@gmail.com>
2026-09-10 13:59:42 +0300

    update Google Analytics

--- src/components/common/AnalyticsTracker.jsx
+++ src/components/common/AnalyticsTracker.jsx
@@ -1,13 +1,16 @@
 import { useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
-import ReactGA from 'react-ga4';
 
 const AnalyticsTracker = () => {
   const location = useLocation();
 
   useEffect(() => {
-    // Send a pageview every time the URL path changes
-    ReactGA.send({ hitType: 'pageview', page: location.pathname + location.search });
+    // Check if gtag is available and send pageview on route change
+    if (typeof window.gtag === 'function') {
+      window.gtag('config', 'G-XE3QPHVN5Q', {
+        page_path: location.pathname + location.search,
+      });
+    }
   }, [location]);
 
   return null; // This component does not render anything visually
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import ReactGA from 'react-ga4'
 import './index.css'
 import App from './App.jsx'
-
-// Google Analytics ID
-ReactGA.initialize('G-XE3QPHVN5Q');
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
