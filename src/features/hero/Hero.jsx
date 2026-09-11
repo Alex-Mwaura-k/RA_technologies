9617b72dffc1ec103ce81c14c46b3ea830fa70d0
Alex Mwaura <alecsmwaura@gmail.com>
2026-09-11 21:23:12 +0300

    Final Hero Update

--- src/features/hero/Hero.jsx
+++ src/features/hero/Hero.jsx
@@ -31,7 +31,7 @@ const Hero = () => {
         - Mobile, Tablet & Laptops (< 1536px / 2xl): 100dvh fills full viewport height.
         - Big Desktop Monitors (>= 1536px / 2xl): Fixed height of 740px.
       */}
-      <section className="relative w-full h-full flex flex-col justify-center overflow-hidden pt-6 pb-4 min-[992px]:pt-20 min-[992px]:pb-6">
+      <section className="relative w-full h-full flex flex-col justify-center overflow-hidden pb-4 min-[992px]:pt-20 min-[992px]:pb-6">
         
         <div className="relative max-w-[1300px] mx-auto px-4 sm:px-6 lg:px-8 w-full z-10 h-full flex flex-col justify-center">
           <div className="grid min-[992px]:grid-cols-2 gap-8 lg:gap-10 items-center h-full max-h-[750px] min-[992px]:max-h-none">
